19eb07162f722584b464ac130b379252653e2ab0
Juliandev02 <julian@strawberryfoundations.org>
2026-06-08 21:42:45 +0200

    chore: add versioning guidelines to Changelog.kt

--- app/src/main/java/org/strawberryfoundations/wear/reply/core/Changelog.kt
+++ app/src/main/java/org/strawberryfoundations/wear/reply/core/Changelog.kt
@@ -6,6 +6,12 @@ data class ChangelogEntry(
     val changes: List<String>
 )
 
+/// This project uses 'semver' to manage its versioning. (MAJOR.MINOR.PATCH)
+/// MAJOR version when you make incompatible API changes,
+/// MINOR version when you add functionality in a backwards-compatible manner,
+/// PATCH version when you make backwards-compatible bug fixes.
+
+
 object Changelog {
     val entries = listOf(
         ChangelogEntry(
